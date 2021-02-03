130bcf72291a02119930496f6e93aa4ca4dec395
jacobkeay <jacobkeay@outlook.com>
2021-02-03 15:10:11 +0000

    Changed to vercel url for temporary deployment

--- config/index.js
+++ config/index.js
@@ -1,3 +1,5 @@
 const dev = process.env.PROD_ENV !== "production";
 
-export const server = dev ? "http://localhost:3000" : "https://ystt.co.uk";
+export const server = dev
+  ? "http://localhost:3000"
+  : "https://nextjs-ystt-site.vercel.app";
